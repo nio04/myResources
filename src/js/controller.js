b54b6d0a56f309f71e23edffd788ea07bab79905
nio04 <nishatislam3108@gmail.com>
2023-10-02 00:52:13 +0600

    /html -added classes related to NAV && /css -refactor nav design && /js -implemented MVC for navigation -works fully

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,8 +1,12 @@
 'use strict';
-
+import { info } from 'autoprefixer';
+import * as navView from './views/navView';
 import icons from 'url:../img/sprite.svg';
 
-const mainHomeContainer = document.querySelector('.main-home__container');
+const mainHomeParent = document.querySelector('#main__home--container');
+const navBtnContainer = document.querySelector('#nav__btn--container');
+
+// const mainHomeContainer = document.querySelector('.main-home__container');
 const mainHomeSideNavContainer = document.querySelector('.side-nav__container');
 const mainHomeSideNavBtn = document.querySelector('.sideNav__Btn');
 const mainHomeMainSection = document.querySelector('.main-home-resource__container');
@@ -52,201 +56,210 @@ const contentFooter = document.querySelector('footer');
 // 	}
 // };
 
+// new operation!!!!!!!!!!
+if (mainHomeParent) {
+	navView.initNavBtnManager();
+}
+
+if (contentPageContainer) {
+	navView.initNavBtnManager();
+}
+
 ////////////////////////
 // MAIN HOME PAGE - NAVIGATION BUTTON
-if (mainHomeContainer) {
-	const sideNavCallback = ([entries]) => {
-		// const btnContainer = mainHomeSideNavBtn.closest('button');
+// if (mainHomeContainer) {
+// 	const sideNavCallback = ([entries]) => {
+// 		// const btnContainer = mainHomeSideNavBtn.closest('button');
 
-		const showButtonManager = (input) => {
-			if (input === 'show') {
-				mainHomeSideNavBtn.classList.add('util-sticky');
-				mainHomeSideNavBtn.classList.remove('util-display-n');
-			} else {
-				mainHomeSideNavBtn.classList.remove('util-sticky');
-				mainHomeSideNavBtn.classList.add('util-display-n');
-			}
-		};
+// 		const showButtonManager = (input) => {
+// 			if (input === 'show') {
+// 				mainHomeSideNavBtn.classList.add('util-sticky');
+// 				mainHomeSideNavBtn.classList.remove('util-display-n');
+// 			} else {
+// 				mainHomeSideNavBtn.classList.remove('util-sticky');
+// 				mainHomeSideNavBtn.classList.add('util-display-n');
+// 			}
+// 		};
 
-		if (entries.isIntersecting) {
-			showButtonManager('show');
-		}
-		if (!entries.isIntersecting) {
-			showButtonManager('hide');
-		}
-	};
+// 		if (entries.isIntersecting) {
+// 			showButtonManager('show');
+// 		}
+// 		if (!entries.isIntersecting) {
+// 			showButtonManager('hide');
+// 		}
+// 	};
 
-	const mainHomeSideNavObserver = new IntersectionObserver(sideNavCallback, {
-		root: null,
-		threshold: 0,
-	});
-	mainHomeSideNavObserver.observe(mainHomeMainSection);
+// 	const mainHomeSideNavObserver = new IntersectionObserver(sideNavCallback, {
+// 		root: null,
+// 		threshold: 0,
+// 	});
+// 	mainHomeSideNavObserver.observe(mainHomeMainSection);
 
-	// MAIN HOME PAGE -- SIDE-NAV-BAR MANAGER
-	mainHomeSideNavBtn.addEventListener('click', (ev) => {
-		let btn = document.querySelector('.sideNav__Btn svg');
-		if (!(ev.target.nodeName === 'svg' || ev.target.nodeName === 'use')) return;
-		else if (ev.target.nodeName === 'use') {
-			btn = ev.target.closest('svg');
-		}
+// 	// MAIN HOME PAGE -- SIDE-NAV-BAR MANAGER
+// 	mainHomeSideNavBtn.addEventListener('click', (ev) => {
+// 		let btn = document.querySelector('.sideNav__Btn svg');
+// 		if (!(ev.target.nodeName === 'svg' || ev.target.nodeName === 'use')) return;
+// 		else if (ev.target.nodeName === 'use') {
+// 			btn = ev.target.closest('svg');
+// 		}
 
-		const openNav = (input) => {
-			if (input === 'close') {
-				btn.classList.remove('close');
-				btn.classList.add('open');
-				btn.innerHTML = `<use xlink:href="${icons}#navOpen"></use>`;
-				mainHomeSideNavContainer.classList.remove('noTransform--js');
+// 		const openNav = (input) => {
+// 			if (input === 'close') {
+// 				btn.classList.remove('close');
+// 				btn.classList.add('open');
+// 				btn.innerHTML = `<use xlink:href="${icons}#navOpen"></use>`;
+// 				mainHomeSideNavContainer.classList.remove('noTransform--js');
 
-				overlay.classList.remove('util-display-y');
-			} else {
-				btn.classList.remove('open');
-				btn.classList.add('close');
-				btn.innerHTML = `<use xlink:href="${icons}#navClose"></use>`;
-				mainHomeSideNavContainer.classList.add('noTransform--js');
+// 				overlay.classList.remove('util-display-y');
+// 			} else {
+// 				btn.classList.remove('open');
+// 				btn.classList.add('close');
+// 				btn.innerHTML = `<use xlink:href="${icons}#navClose"></use>`;
+// 				mainHomeSideNavContainer.classList.add('noTransform--js');
 
-				overlay.classList.add('util-display-y');
-				overlay.addEventListener('click', () => {
-					openNav('close');
-				});
-			}
-		};
+// 				overlay.classList.add('util-display-y');
+// 				overlay.addEventListener('click', () => {
+// 					openNav('close');
+// 				});
+// 			}
+// 		};
 
-		if (btn.classList.value === 'open') {
-			openNav('open');
+// 		if (btn.classList.value === 'open') {
+// 			openNav('open');
 
-			document.addEventListener('keydown', (ev) => {
-				if (ev.key === 'Escape') openNav('close');
-			});
-		} else {
-			openNav('close');
-		}
-	});
-}
+// 			document.addEventListener('keydown', (ev) => {
+// 				if (ev.key === 'Escape') openNav('close');
+// 			});
+// 		} else {
+// 			openNav('close');
+// 		}
+// 	});
+// }
 
 /////////////////
 // CONTENT PAGE --STICKY NAV IMPLEMENT
-if (contentPageContainer) {
-	const stickyNav = ([entries]) => {
-		const contentNavParent = contentNavBtn.closest('.content__nav--btn');
-		const navButtonMananger = (input) => {
-			if (input === 'open') {
-				contentNav.classList.add('stickyNav--js');
-				contentNavParent.classList.add('util-opacity-1');
-				contentNavParent.classList.remove('util-opacity-0');
-			} else {
-				contentNav.classList.remove('stickyNav--js');
-				contentNavParent.classList.add('util-opacity-0');
-				contentNavParent.classList.remove('util-opacity-1');
-			}
-		};
+// if (contentPageContainer) {
+// 	const stickyNav = ([entries]) => {
+// 		const contentNavParent = contentNavBtn.closest('.content__nav--btn');
+// 		const navButtonMananger = (input) => {
+// 			if (input === 'open') {
+// 				contentNav.classList.add('stickyNav--js');
+// 				contentNavParent.classList.add('util-opacity-1');
+// 				contentNavParent.classList.remove('util-opacity-0');
+// 			} else {
+// 				contentNav.classList.remove('stickyNav--js');
+// 				contentNavParent.classList.add('util-opacity-0');
+// 				contentNavParent.classList.remove('util-opacity-1');
+// 			}
+// 		};
 
-		if (!entries.isIntersecting) {
-			navButtonMananger('open');
+// 		if (!entries.isIntersecting) {
+// 			navButtonMananger('open');
 
-			const btnState = document.querySelector('.content__nav--btn svg');
+// 			const btnState = document.querySelector('.content__nav--btn svg');
 
-			const navShowManager = (input) => {
-				if (input === 'open') {
-					contentNav.classList.remove('noTransform--js');
-					btnState.classList.remove('close');
-					btnState.classList.add('open');
-					btnState.innerHTML = `<use xlink:href="${icons}#navOpen"></use>`;
+// 			const navShowManager = (input) => {
+// 				if (input === 'open') {
+// 					contentNav.classList.remove('noTransform--js');
+// 					btnState.classList.remove('close');
+// 					btnState.classList.add('open');
+// 					btnState.innerHTML = `<use xlink:href="${icons}#navOpen"></use>`;
 
-					// overlayRender(contentPageContainer, 'overlayRemove');
-					// overlays.forEach((el) => {
-					// 	el.classList.remove('util-display-y');
-					// });
-					overlay.classList.remove('util-display-y');
+// 					// overlayRender(contentPageContainer, 'overlayRemove');
+// 					// overlays.forEach((el) => {
+// 					// 	el.classList.remove('util-display-y');
+// 					// });
+// 					overlay.classList.remove('util-display-y');
 
-					contentFloatButtons.forEach((el) => {
-						el.classList.remove('util-display-n');
-					});
-				} else {
-					btnState.classList.remove('open');
-					btnState.classList.add('close');
-					btnState.innerHTML = `<use xlink:href="${icons}#navClose"></use>`;
-					contentNav.classList.add('noTransform--js');
+// 					contentFloatButtons.forEach((el) => {
+// 						el.classList.remove('util-display-n');
+// 					});
+// 				} else {
+// 					btnState.classList.remove('open');
+// 					btnState.classList.add('close');
+// 					btnState.innerHTML = `<use xlink:href="${icons}#navClose"></use>`;
+// 					contentNav.classList.add('noTransform--js');
 
-					// overlayRender(contentPageContainer, 'overlayShow', navShowManager, 'open');
-					// overlays.forEach((el) => {
-					// 	el.classList.add('util-display-y');
-					// 	el.addEventListener('click', () => {
-					// 		navShowManager('open');
-					// 	});
-					// });
-					contentNavBtn.classList.add('util-display-y');
-					overlay.addEventListener('click', () => {
-						navShowManager('open');
-					});
+// 					// overlayRender(contentPageContainer, 'overlayShow', navShowManager, 'open');
+// 					// overlays.forEach((el) => {
+// 					// 	el.classList.add('util-display-y');
+// 					// 	el.addEventListener('click', () => {
+// 					// 		navShowManager('open');
+// 					// 	});
+// 					// });
+// 					contentNavBtn.classList.add('util-display-y');
+// 					overlay.addEventListener('click', () => {
+// 						navShowManager('open');
+// 					});
 
-					contentFloatButtons.forEach((el) => {
-						el.classList.add('util-display-n');
-					});
-				}
-			};
+// 					contentFloatButtons.forEach((el) => {
+// 						el.classList.add('util-display-n');
+// 					});
+// 				}
+// 			};
 
-			contentNavParent.addEventListener('click', () => {
-				if (btnState.classList.value === 'open') {
-					navShowManager('close');
+// 			contentNavParent.addEventListener('click', () => {
+// 				if (btnState.classList.value === 'open') {
+// 					navShowManager('close');
 
-					document.addEventListener('keydown', (ev) => {
-						if (ev.key === 'Escape') navShowManager('open');
-					});
-				} else {
-					navShowManager('open');
-				}
-			});
+// 					document.addEventListener('keydown', (ev) => {
+// 						if (ev.key === 'Escape') navShowManager('open');
+// 					});
+// 				} else {
+// 					navShowManager('open');
+// 				}
+// 			});
 
-			// overlayRender(contentPageContainer, 'overlayShow', 'navShowManager', 'open');
-			// overlays.forEach((el) => {
-			// 	el.addEventListener('click', () => {
-			// 		navShowManager('open');
-			// 	});
-			// });
-			overlay.addEventListener('clck', () => {
-				navShowManager('open');
-			});
-		}
-		if (entries.isIntersecting) {
-			navButtonMananger('off');
-		}
-	};
-	const contentStickyNavObs = new IntersectionObserver(stickyNav, {
-		root: null,
-		threshold: 0,
-		rootMargin: '100px',
-	});
-	contentStickyNavObs.observe(contentMainHeader);
-}
+// 			// overlayRender(contentPageContainer, 'overlayShow', 'navShowManager', 'open');
+// 			// overlays.forEach((el) => {
+// 			// 	el.addEventListener('click', () => {
+// 			// 		navShowManager('open');
+// 			// 	});
+// 			// });
+// 			overlay.addEventListener('clck', () => {
+// 				navShowManager('open');
+// 			});
+// 		}
+// 		if (entries.isIntersecting) {
+// 			navButtonMananger('off');
+// 		}
+// 	};
+// 	const contentStickyNavObs = new IntersectionObserver(stickyNav, {
+// 		root: null,
+// 		threshold: 0,
+// 		rootMargin: '100px',
+// 	});
+// 	contentStickyNavObs.observe(contentMainHeader);
+// }
 
 // CONTENT PAGE - STICKY NAV - POST DROP DOWN MENU
-if (contentPageContainer) {
-	contentPostMenuBtn.addEventListener('click', () => {
-		const button = document.querySelector('.dropdownMenu__btn svg');
+// if (contentPageContainer) {
+// 	contentPostMenuBtn.addEventListener('click', () => {
+// 		const button = document.querySelector('.dropdownMenu__btn svg');
 
-		const showArrowButton = (input) => {
-			const elWrap = document.querySelector('.nav__element-wrap');
+// 		const showArrowButton = (input) => {
+// 			const elWrap = document.querySelector('.nav__element-wrap');
 
-			if (input === 'up') {
-				button.innerHTML = `<use xlink:href="${icons}#arrowUp"></use>`;
-				button.classList.remove('arrowDown');
-				button.classList.add('arrowUp');
-				elWrap.classList.add('noTransform--js');
-			} else {
-				button.innerHTML = `<use xlink:href="${icons}#arrowDown"></use>`;
-				button.classList.remove('arrowUp');
-				button.classList.add('arrowDown');
-				elWrap.classList.remove('noTransform--js');
-			}
-		};
-		// CHANGE BUTTON + CLASS NAME
-		if (button.classList.contains('arrowDown')) {
-			showArrowButton('up');
-		} else {
-			showArrowButton('down');
-		}
-	});
-}
+// 			if (input === 'up') {
+// 				button.innerHTML = `<use xlink:href="${icons}#arrowUp"></use>`;
+// 				button.classList.remove('arrowDown');
+// 				button.classList.add('arrowUp');
+// 				elWrap.classList.add('noTransform--js');
+// 			} else {
+// 				button.innerHTML = `<use xlink:href="${icons}#arrowDown"></use>`;
+// 				button.classList.remove('arrowUp');
+// 				button.classList.add('arrowDown');
+// 				elWrap.classList.remove('noTransform--js');
+// 			}
+// 		};
+// 		// CHANGE BUTTON + CLASS NAME
+// 		if (button.classList.contains('arrowDown')) {
+// 			showArrowButton('up');
+// 		} else {
+// 			showArrowButton('down');
+// 		}
+// 	});
+// }
 /////////////
 // CONTENT PAGE -- STICKY NAV - DAY/NIGHT MODE CHANGER
 if (contentPageContainer) {
