70811553f9fefe63eb2c1fa912ad24c1aad2b18d
nio04 <nishatislam3108@gmail.com>
2023-10-03 00:22:08 +0600

    /html -added class on <main> for btns intersection && /js -intersection for btns are full functional

---
+++ src/js/views/visibleBtnView.js
@@ -0,0 +1,37 @@
+const target = document.querySelector('#content__main--section');
+const navBtnContainer = document.querySelector('.content__nav--btn');
+const navigatorBtns = document.querySelectorAll('.float__btn--container');
+
+const btnsIntersection = new IntersectionObserver(navViewIntersection, {
+	root: null,
+	threshold: 0,
+	rootMargin: '-300px',
+});
+
+btnsIntersection.observe(target);
+
+function navViewIntersection([entries]) {
+	if (entries.isIntersecting) {
+		// SHOW NAVIGATOR BUTTON
+		navBtnContainer.classList.add('util-display-y');
+		// SHOW NAVIGATROR [TOP, BOTTOM] BUTTON
+		showNavigatorBtns();
+	} else {
+		// HIDE NAVIGATOR BUTTON
+		navBtnContainer.classList.remove('util-display-y');
+		// HIDE NAVIGATROR [TOP, BOTTOM] BUTTON
+		hideNavigatorBtns();
+	}
+}
+
+function showNavigatorBtns() {
+	navigatorBtns.forEach((el) => {
+		el.classList.add('util-show-element-transform');
+	});
+}
+
+function hideNavigatorBtns() {
+	navigatorBtns.forEach((el) => {
+		el.classList.remove('util-show-element-transform');
+	});
+}
