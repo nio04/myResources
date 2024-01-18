fded66a6a3a0413262da63c92451bd57fa6e4339
nio04 <nishatislam3108@gmail.com>
2024-01-19 00:49:55 +0600

    rename background video file name && fix its entry path accordingly

--- src/js/views/responsiveVideo.js
+++ src/js/views/responsiveVideo.js
@@ -2,11 +2,11 @@ const video = document.querySelector('.mainhome__header__background video source
 
 export function videoResponsive() {
 	if (window.innerWidth >= 910) {
-		video.src = '../asset/video/mainHomeHeader_1080.mp4';
+		video.src = '../asset/video/mainHomeHeader_1_1080.mp4';
 	} else if (window.innerWidth >= 670) {
-		video.src = '../asset/video/mainHomeHeader_720.mp4';
+		video.src = '../asset/video/mainHomeHeader_1_720.mp4';
 	} else if (window.innerWidth < 670) {
-		video.src = '../asset/video/mainHomeHeader_540.mp4';
+		video.src = '../asset/video/mainHomeHeader_1_540.mp4';
 	}
 }
 
