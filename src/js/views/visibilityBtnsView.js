80a4b22f93b4029c4c2f0c56bc5a75b818f07fdf
nio04 <nishatislam3108@gmail.com>
2024-02-04 17:23:58 +0600

    fixed mainHome anim timing, content page footer layout, js- btn visibilty issue

--- src/js/views/visibilityBtnsView.js
+++ src/js/views/visibilityBtnsView.js
@@ -1,20 +1,21 @@
 const contentPage = document.querySelector('.content__page--container');
-const target = document.querySelector('#content__main--section');
+const target = document.querySelector(".nav--container");
 const navBtnContainer = document.querySelector('.content__nav--btn');
 const navigatorBtns = document.querySelectorAll('.float__btn--container');
 const magicBtn = document.querySelector('.contentPage__magic-btn svg');
 
+const header=document.querySelector(".nav--container")
+
 const btnsIntersection = new IntersectionObserver(navViewIntersection, {
 	root: null,
 	threshold: 0,
-	rootMargin: '-300px',
 });
 
 if (contentPage) btnsIntersection.observe(target);
 
 function navViewIntersection([entries]) {
-	if (entries.isIntersecting) {
-		// SHOW NAVIGATOR BUTTON
+	if (!entries.isIntersecting) {
+			// SHOW NAVIGATOR BUTTON
 		navBtnContainer.classList.add('util-display-y');
 		// SHOW NAVIGATROR [TOP, BOTTOM] BUTTON
 		showNavigatorBtns();
